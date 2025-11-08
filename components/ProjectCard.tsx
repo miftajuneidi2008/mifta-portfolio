eb3867996bfc4b4deaab5d650b0a6c77ef999ed4
unknown <DELL 7020@ZZB-TRANS-PC1.zamzambank.local>
2025-11-08 11:56:21 +0300

    project grid

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,15 +1,42 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
 import React from "react";
+import Links from "@/assets/icons/akar-icons_link-chain.svg";
+import Github from "@/assets/icons/akar-icons_github-fill.svg";
 type cardProps = {
   title: string;
   description: string;
   stack: string;
-  image: string;
+  image: string | StaticImageData;
 };
 const ProjectCard = ({ title, description, stack, image }: cardProps) => {
   return (
-    <div className="flex flex-col rounded-md">
-      <Image src={image} height={400} width={400} alt={title} />
+    <div className="flex flex-col rounded-lg">
+      <Image
+        src={image}
+        height={400}
+        width={400}
+        alt={title}
+        className="h-64 rounded-tl-lg rounded-tr-lg"
+      />
+      <div className="flex flex-col gap-2 px-4">
+        <h3 className="font-sm font-semibold">{title}</h3>
+        <p className="text-sm text-[#666666]">{description}</p>
+        <div>
+          <span className="text-md font-semibold">Tech Stack</span>
+          {stack}
+        </div>
+        <div className="flex justify-between">
+          <Link href={`#`} className="flex items-center gap-2">
+            <Links className="h-5 w-5" />
+            <span className="underline">Live Preview</span>
+          </Link>
+          <Link href={`#`} className="flex items-center gap-2">
+            <Github className="h-5 w-5" />
+            View Code
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
